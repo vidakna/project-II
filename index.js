1ea54ae02c3635a117bd3e03a3df70487362ff11
ShimalkaYohan <101331942+ShimalkaYohan@users.noreply.github.com>
2023-05-13 16:30:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 app.use("/", (req, res) => {
-    res.send("Hello from server side");
+    res.send("Hello ss from server side");
 });
 app.listen(PORT, () => {
     console.log('Server is running  at PORT ${PORT}');
