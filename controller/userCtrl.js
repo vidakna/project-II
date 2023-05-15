182256e5f73586721ee8046c6f025be2830487a5
vidakna <75603774+vidakna@users.noreply.github.com>
2023-05-15 13:40:28 +0530

    Initial commit

---
+++ controller/userCtrl.js
@@ -0,0 +1,161 @@
+const { generateToken } = require("../config/jwtToken");
+const User = require("../models/userMOdel");
+const asyncHandler = require("express-async-handler");
+
+
+const createUser = asyncHandler(async(req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email: email });
+    if (!findUser) {
+        //create a new user
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } else {
+        throw new Error("User Already Exists");
+    }
+});
+const loginUserCtrl = asyncHandler(async(req, res) => {
+    const { email, password } = req.body;
+    //check if user exists or not
+    const findUser = await User.findOne({ email });
+    if (findUser && await findUser.isPasswordMatched(password)) {
+        res.json({
+            _id: findUser._id,
+            firstname: findUser.firstname,
+            lastname: findUser.lastname,
+            email: findUser.email,
+            mobile: findUser.mobile,
+            token: generateToken(findUser._id),
+        });
+
+    } else {
+        throw new Error("Invalid Credentials");
+    }
+});
+
+//Update a user
+// const updatedUser = asyncHandler(async(req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(
+//             id, {
+//                 firstname: req ? .body ? .firstname,
+//                 lastname: req ? .body ? .lastname,
+//                 email: req ? .body ? .email,
+//                 mobile: req ? .body ? .mobile,
+//             }, {
+//                 new: true,
+//             }
+//         );
+//         res.json(updatedUser);
+//     } catch (error) {
+//         throw new Error(error);
+//     }
+// });
+
+const updatedUser = asyncHandler(async(req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id, {
+                firstname: req.body ? req.body.firstname : undefined,
+                lastname: req.body ? req.body.lastname : undefined,
+                email: req.body ? req.body.email : undefined,
+                mobile: req.body ? req.body.email : undefined,
+            }, {
+                new: true,
+            }
+        );
+        res.json(updatedUser);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
+
+//Get all users
+const getallUser = asyncHandler(async(req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+
+    }
+});
+// module.exports = { createUser };
+
+// const User = require("../models/userMOdel");
+
+// const createUser = async(req, res) => {
+//     const email = req.body.email;
+//     const findUser = await User.find({ email: email });
+//     if (!findUser) {
+//         //create a new user
+//         const newUser = await User.create(req.body);
+//         res.json(newUser);
+//     } else {
+//         res.json({
+//             msg: "User Already Exists",
+//             success: false,
+//         });
+//     }
+// };
+
+// module.exports = { createUser };
+
+// const User = require("../models/userMOdel");
+
+// const createUser = async(req, res) => {
+//     const email = req.body.email;
+//     try {
+//         const user = await User.findOne({ email });
+//         if (user) {
+//             return res.json({
+//                 msg: "User Already Exists",
+//                 success: false,
+//             });
+//         }
+//         const newUser = await User.create(req.body);
+//         res.json(newUser);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//             msg: "Server Error",
+//             success: false,
+//         });
+//     }
+// };
+
+//Get a single user
+
+const getaUser = asyncHandler(async(req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    try {
+        const getaUser = await User.findById(id);
+        res.json({
+            getaUser,
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//Delete user
+
+const deleteaUser = asyncHandler(async(req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    try {
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteaUser,
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser, };
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const dbConnect = require("./config/dbconnect");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+
+dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running  at PORT ${PORT}`);
+});
+
+// const express = require("express");
+// const dbConnect = require("./config/dbconnect");
+// const app = express();
+// const dotenv = require("dotenv").config();
+// const PORT = process.env.PORT || 4000;
+// const authRouter = require("./routes/authRoute");
+// const bodyParser = require("body-parser");
+
+// dbConnect();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use("/api/user", authRouter);
+// app.listen(PORT, () => {
+//     console.log(`Server is running at PORT ${PORT}`);
+// });
+
+app.use("/", (req, res) => {
+    res.send("Hello uiu from server side");
+});
+app.listen(PORT, () => {
+    console.log('Server is running  at PORT ${PORT}');
+});
