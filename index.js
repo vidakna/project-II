089c51839c3c11f7e0c21e76a1bcc4b2b11b4c62
TMIrugalbandara <89934712+TMIrugalbandara@users.noreply.github.com>
2023-05-15 13:50:13 +0530

    test 3

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(PORT, () => {
 // });
 
 app.use("/", (req, res) => {
-    res.send("Hello abbbs from server side");
+    res.send("Hello vidoo from server side");
 });
 app.listen(PORT, () => {
     console.log('Server is running  at PORT ${PORT}');
