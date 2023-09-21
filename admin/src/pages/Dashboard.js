eb85e8e114558162882664f494cfb3202bda5662
isurukavinda99 <isurukavinda999@gmail.com>
2023-09-21 23:33:43 +0530

    bug fix - update income statement

--- admin/src/pages/Dashboard.js
+++ admin/src/pages/Dashboard.js
@@ -13,10 +13,10 @@ const columns = [
     title: "Name",
     dataIndex: "name",
   },
-  {
-    title: "Product",
-    dataIndex: "product",
-  },
+  // {
+  //   title: "Product",
+  //   dataIndex: "product",
+  // },
   {
     title: "Status",
     dataIndex: "staus",
@@ -148,47 +148,47 @@ const Dashboard = () => {
   return (
     <div>
       <h3 className="mb-4 title">Dashboard</h3>
-      <div className="d-flex justify-content-between align-items-center gap-3">
-        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
-          <div>
-            <p className="desc">Total</p>
-            <h4 className="mb-0 sub-title">Rs. 1100</h4>
-          </div>
-          <div className="d-flex flex-column align-items-end">
-            <h6>
-              <BsArrowDownRight /> 32%
-            </h6>
-            <p className="mb-0  desc">Compared To July 2023</p>
+      {/*<div className="d-flex justify-content-between align-items-center gap-3">*/}
+      {/*  <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">*/}
+      {/*    <div>*/}
+      {/*      <p className="desc">Total</p>*/}
+      {/*      <h4 className="mb-0 sub-title">Rs. 1100</h4>*/}
+      {/*    </div>*/}
+      {/*    <div className="d-flex flex-column align-items-end">*/}
+      {/*      <h6>*/}
+      {/*        <BsArrowDownRight /> 32%*/}
+      {/*      </h6>*/}
+      {/*      <p className="mb-0  desc">Compared To July 2023</p>*/}
 
-          </div>
-        </div>
-        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
-          <div>
-            <p className="desc">Total</p>
-            <h4 className="mb-0 sub-title">Rs. 1100</h4>
-          </div>
-          <div className="d-flex flex-column align-items-end">
-            <h6 className="red">
-              <BsArrowDownRight /> 32%
-            </h6>
-            <p className="mb-0  desc">Compared To August 2023</p>
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">*/}
+      {/*    <div>*/}
+      {/*      <p className="desc">Total</p>*/}
+      {/*      <h4 className="mb-0 sub-title">Rs. 1100</h4>*/}
+      {/*    </div>*/}
+      {/*    <div className="d-flex flex-column align-items-end">*/}
+      {/*      <h6 className="red">*/}
+      {/*        <BsArrowDownRight /> 32%*/}
+      {/*      </h6>*/}
+      {/*      <p className="mb-0  desc">Compared To August 2023</p>*/}
 
-          </div>
-        </div>
-        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
-          <div>
-            <p className="desc">Total</p>
-            <h4 className="mb-0 sub-title">Rs. 1100</h4>
-          </div>
-          <div className="d-flex flex-column align-items-end">
-            <h6 className="green">
-              <BsArrowDownRight /> 32%
-            </h6>
-              <p className="mb-0  desc">Compared To July 2023</p>
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">*/}
+      {/*    <div>*/}
+      {/*      <p className="desc">Total</p>*/}
+      {/*      <h4 className="mb-0 sub-title">Rs. 1100</h4>*/}
+      {/*    </div>*/}
+      {/*    <div className="d-flex flex-column align-items-end">*/}
+      {/*      <h6 className="green">*/}
+      {/*        <BsArrowDownRight /> 32%*/}
+      {/*      </h6>*/}
+      {/*        <p className="mb-0  desc">Compared To July 2023</p>*/}
 
-          </div>
-        </div>
-      </div>
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
       <div className="mt-4">
         <h3 className="mb-5 title">Income Statics</h3>
         <div>
