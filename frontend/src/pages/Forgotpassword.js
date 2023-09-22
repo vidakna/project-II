83a1bd0e220823091ded391c1c27b813bb341412
isurukavinda99 <isurukavinda999@gmail.com>
2023-09-22 21:41:07 +0530

    bug fix - reset password

--- frontend/src/pages/Forgotpassword.js
+++ frontend/src/pages/Forgotpassword.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import axios from "axios";
+import {base_url, config} from "../utills/axiosConfig";
 const Forgotpassword = () => {
 
-  const reset = () =>{
-    alert("asdasd")
+  const [email , setEmail] = useState("");
+
+  const reset = (e) =>{
+    e.preventDefault()
+
+    const body  = {
+      "email" : email
+    }
+
+    axios.post(`http://localhost:5000/api/user/password-reset`, body)
+        .then((res) => {
+          alert("PASSWORD CHANGED");
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("SOMETHING WENT WRONG");
+        });
+
+
   }
 
 
@@ -24,7 +43,7 @@ const Forgotpassword = () => {
                 We will send you an email to reset your password
               </p>
               <form onSubmit={reset} action="" className="d-flex flex-column gap-15">
-                <CustomInput type="email" name="email" placeholder="Email" />
+                <CustomInput value={email} onChange ={(e)=>setEmail(e.target.value)} type="email" required = {true} name="email" placeholder="Email" />
 
                 <div>
                   <div className="mt-3 d-flex justify-content-center flex-column gap-15 align-items-center">
